b836a8b76ebc2b96a2b202f252d6011f0f89ac81
Abhishek Manoj Pokale <98730215+Abhi132004@users.noreply.github.com>
2023-11-13 14:16:45 +0530

    Create 14.Longest SubString.cpp

---
+++ 14.Longest SubString.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/submissions/detail/1095948076/
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxLength = 0;
+        int start = 0;
+        unordered_map<char, int> charIndexMap;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (charIndexMap.find(s[i]) != charIndexMap.end() && charIndexMap[s[i]] >=start) {
+                start = charIndexMap[s[i]] + 1;
+            }
+
+            charIndexMap[s[i]] = i;
+            maxLength = max(maxLength, i - start + 1);
+        }
+
+        return maxLength;
+    }
+};
