907850e0104ef2182cd19d37484bbdafbbdc966e
Abhishek Pokale <98730215+Abhi132004@users.noreply.github.com>
2024-05-28 12:20:51 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum cost to fill given weight in a bag/minimum-cost-to-fill-given-weight-in-a-bag.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+class Solution {
+public:
+    int minimumCost(int n, int w, std::vector<int>& cost) {
+        // Create a DP array to store the minimum cost for each weight
+        std::vector<int> dp(w + 1, INT_MAX);
+        dp[0] = 0;  // The cost to buy 0 kg is 0
+
+        // Loop through each weight capacity from 1 to w
+        for (int i = 1; i <= w; ++i) {
+            // Try every packet size from 1 kg to n kg
+            for (int j = 1; j <= n; ++j) {
+                // If the packet size is j kg and it is available
+                if (j <= i && cost[j - 1] != -1) {
+                    // If using this packet size leads to a lower cost
+                    if (dp[i - j] != INT_MAX) {
+                        dp[i] = std::min(dp[i], dp[i - j] + cost[j - 1]);
+                    }
+                }
+            }
+        }
+
+        return dp[w] == INT_MAX ? -1 : dp[w];
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        int w;
+        scanf("%d", &w);
+
+        vector<int> cost(n);
+        Array::input(cost, n);
+
+        Solution obj;
+        int res = obj.minimumCost(n, w, cost);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
