6c7754f9b4618bffad435f1f1a0efcab53ca5990
Abhishek Pokale <98730215+Abhi132004@users.noreply.github.com>
2024-06-24 23:21:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search Pattern (Z-algorithm)/search-pattern-zalgorithm.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        vector <int> search(string pat, string txt)
+        {
+            //code hee.
+            int patLen = pat.length();
+            int txtLen = txt.length();
+            
+            vector<int> ans;
+            for(int i = 0;i<txtLen-patLen+1;i++){
+                if(pat == txt.substr(i,patLen)){
+                    ans.push_back(i+1);
+                }
+            }
+            
+            return ans;
+            
+        }
+     
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        if(res.size()==0)
+            cout<<"-1 ";
+        else
+            for (int i : res)
+                cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
